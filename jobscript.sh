6dbbaadf43d5a2e80f022ebbba4adf9b357ae6bf
ibro45 <ibrahimhadzic45@gmail.com>
2019-11-14 16:01:44 +0100

    bob

--- jobscript.sh
+++ jobscript.sh
@@ -40,4 +40,4 @@ pip install --user -r requirements.txt
 
 #python3 train.py --niter 20 --niter_decay 20 --save_epoch_freq 10 --dataset_mode npy_aligned_3d --dataroot ../3D_460_patchified_norm/ --model paired_revgan3d --name 3d_460 --which_model_netG edsrF_generator_3d --gpu_ids 0,1 --batchSize 2 --which_model_netD n_layers --n_layers_D 2 --lr_G 0.0001 --lr_D 0.0004
 
-python3 train.py --niter 100 --niter_decay 100 --save_epoch_freq 10 --dataset_mode npy_aligned_2d --dataroot ../2D_460_norm/ --model paired_revgan --name 2d_460 --gpu_ids 0,1 --batchSize 32 --lr_G 0.0001 --lr_D 0.0004
+python3 train.py --niter 100 --niter_decay 100 --save_epoch_freq 10 --dataset_mode npy_aligned_2d --dataroot ../2D_460_norm/ --model paired_revgan --name 2d_460 --gpu_ids 0,1 --batchSize 16 --lr_G 0.0001 --lr_D 0.0004
