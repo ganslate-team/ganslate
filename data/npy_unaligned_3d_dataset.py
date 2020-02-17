95a80d0c69a1cd1872c355ce7231125ca4ea650a
ibro45 <ibrahimhadzic45@gmail.com>
2020-02-17 02:45:41 +0100

    focused random patches

--- data/npy_unaligned_3d_dataset.py
+++ data/npy_unaligned_3d_dataset.py
@@ -17,30 +17,62 @@ def normalize(image, MIN_B=-1024.0, MAX_B=3072.0):
     image = (image - MIN_B) / (MAX_B - MIN_B)
     return 2*image - 1
 
-def random_patch_3d(volume, min_value, patch_size=(64,64,64), threshold=0.6):
+
+def focused_random_zxy(zxy, window, valid_region):
+    selected_zxy = []
+    # for each axis
+    for idx in range(len(zxy)):
+        # find the lowest and highest position between which to focus for this axis
+        min_position = int(zxy[idx] - window[idx])
+        max_position = int(zxy[idx] + window[idx])
+        # if one of the boundaries of the focus is outside of the possible area to sample from, cap it
+        min_position = max(0, min_position)
+        max_position = min(max_position, valid_region[idx])
+        selected_zxy.append(random.randint(min_position, max_position))
+    return selected_zxy
+
+def random_patch_3d(volume, patch_size=(64,64,64), min_value=-1024, threshold=0.6, focus_around_zxy=None):
+    '''
+     volume:           whole CT scan (numpy array)
+     patch_size:       size of the 3D volume to be extracted from the original volume
+     min_value:        used for prevent taking patches that contain too many voxels of that value or lower
+     threshold:        defines allowed proportion of voxels in a volume with values lower or equal to min_value
+     focus_around_zxy: enables taking a patch from B that is in a similar location to the patch from A
+    '''
     patch_shape = np.array(patch_size)
     volume_shape = np.array(volume.shape[-3:])
-    # a patch can have a starting coordinate anywhere from 
-    # where it can fit with the defined patch size
+    # a patch can have a starting coordinate anywhere from where it can fit with the defined patch size
     valid_starting_region = volume_shape - patch_shape
 
-    # pick a random starting point in valid region of volume
-    z = random.randint(0, valid_starting_region[0])
-    x = random.randint(0, valid_starting_region[1])
-    y = random.randint(0, valid_starting_region[2])
+    if focus_around_zxy is None:
+        # pick a random starting point in valid region of volume
+        z = random.randint(0, valid_starting_region[0])
+        x = random.randint(0, valid_starting_region[1])
+        y = random.randint(0, valid_starting_region[2])
+
+    else: # take a relative neighbor of patch A in patch B
+        # 3D window/neighborhood of focus_around_zxy from which will be randomly selected a new start zxy for B
+        # TODO: focus window, define 0.2 through base_option
+        focus_window = np.multiply(volume_shape, 0.2).astype(np.int64)
+        # the starting position from A is given in relative form (A_start_zxy / A_shape)
+        zxy = np.array(focus_around_zxy) * volume_shape  # find start position of A translated in B
+        z, x, y = focused_random_zxy(zxy, focus_window, valid_starting_region)
+        
 
     # extract the patch from the volume
     patch = volume[z:z+patch_size[0],
                    x:x+patch_size[1],
                    y:y+patch_size[2]]
 
-    # ratio: number of completely black voxels / total number of voxels 
-    # E.g. min_value is -1024 (black)
+    # ratio: number of completely black voxels / total number of voxels, e.g. min_value is -1024 (black)
     black_voxels_ratio = np.count_nonzero(patch <= min_value) / patch.numel()
-    # if above threshold, find another patch 
     if black_voxels_ratio > threshold:
-        return random_patch_3d(volume, min_value, patch_size)
-    return patch
+        # if above threshold, find another patch 
+        return random_patch_3d(volume, patch_size, min_value, threshold, focus_around_zxy)
+    
+    # used only for focus_around_zxy
+    relative_zxy = (np.array([z,x,y]) / volume_shape).tolist()
+    return patch, relative_zxy
 
 class NpyUnaligned3dDataset(BaseDataset):
     @staticmethod
@@ -77,15 +109,16 @@ class NpyUnaligned3dDataset(BaseDataset):
         B = torch.Tensor(B)
 
         # random patch extraction
-        A = random_patch_3d(A, 
-                            min_value=self.norm_A["min"], 
-                            patch_size=self.opt.patch_size, 
-                            threshold=self.opt.threshold_black_voxels)
+        A, A_zxy = random_patch_3d(A, 
+                                  patch_size=self.opt.patch_size,
+                                  min_value=self.norm_A["min"],  
+                                  threshold=self.opt.threshold_black_voxels)
 
-        B = random_patch_3d(B, 
-                            min_value=self.norm_B["min"], 
-                            patch_size=self.opt.patch_size, 
-                            threshold=self.opt.threshold_black_voxels)
+        B, _ = random_patch_3d(B, 
+                               patch_size=self.opt.patch_size,
+                               min_value=self.norm_B["min"],  
+                               threshold=self.opt.threshold_black_voxels,
+                               focus_around_zxy=A_zxy)
 
         # normalization
         A = normalize(A, self.norm_A["min"], self.norm_A["max"])
