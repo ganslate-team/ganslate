62fe70b907cbc06d9817059707d1ee267f96fb47
ibro45 <ibrahimhadzic45@gmail.com>
2020-05-01 02:06:55 +0200

    NormLayer selection for VNet generator

--- models/generators/vnet.py
+++ models/generators/vnet.py
@@ -11,21 +11,21 @@ class VNet(nn.Module):
         norm_layer = get_norm_layer(norm_type)
         use_bias = is_bias_before_norm(norm_type)
 
-        self.in_ab = InputBlock(start_n_filters) 
-        self.in_ba = InputBlock(start_n_filters)
+        self.in_ab = InputBlock(start_n_filters, norm_layer, use_bias) 
+        self.in_ba = InputBlock(start_n_filters, norm_layer, use_bias)
 
-        self.down1 = DownBlock(start_n_filters, 1, keep_input)
-        self.down2 = DownBlock(start_n_filters*2, 2, keep_input) 
-        self.down3 = DownBlock(start_n_filters*4, 3, keep_input) 
-        self.down4 = DownBlock(start_n_filters*8, 2, keep_input) 
+        self.down1 = DownBlock(start_n_filters, 1, keep_input, norm_layer, use_bias)
+        self.down2 = DownBlock(start_n_filters*2, 2, keep_input, norm_layer, use_bias) 
+        self.down3 = DownBlock(start_n_filters*4, 3, keep_input, norm_layer, use_bias) 
+        self.down4 = DownBlock(start_n_filters*8, 2, keep_input, norm_layer, use_bias) 
 
-        self.up4 = UpBlock(start_n_filters*16, start_n_filters*16, 2, keep_input) 
-        self.up3 = UpBlock(start_n_filters*16, start_n_filters*8, 2, keep_input) 
-        self.up2 = UpBlock(start_n_filters*8, start_n_filters*4, 1, keep_input) 
-        self.up1 = UpBlock(start_n_filters*4, start_n_filters*2, 1, keep_input) 
+        self.up4 = UpBlock(start_n_filters*16, start_n_filters*16, 2, keep_input, norm_layer, use_bias) 
+        self.up3 = UpBlock(start_n_filters*16, start_n_filters*8, 2, keep_input, norm_layer, use_bias) 
+        self.up2 = UpBlock(start_n_filters*8, start_n_filters*4, 1, keep_input, norm_layer, use_bias) 
+        self.up1 = UpBlock(start_n_filters*4, start_n_filters*2, 1, keep_input, norm_layer, use_bias) 
         
-        self.out_ab = OutBlock(start_n_filters*2) 
-        self.out_ba = OutBlock(start_n_filters*2)
+        self.out_ab = OutBlock(start_n_filters*2, norm_layer, use_bias) 
+        self.out_ba = OutBlock(start_n_filters*2, norm_layer, use_bias)
 
     def forward(self, x, inverse=False):
         if inverse:
@@ -49,34 +49,34 @@ class VNet(nn.Module):
 
 class InvertibleBlock(nn.Module):
     # TODO: is it possible to pass in a constructed block and make it invertible? The class could be reusable for other architectures
-    def __init__(self, n_channels, keep_input):
+    def __init__(self, n_channels, keep_input, norm_layer, use_bias):
         super(InvertibleBlock, self).__init__()
         
         invertible_module = memcnn.AdditiveCoupling(
-            Fm=self.build_conv_block(n_channels//2),
-            Gm=self.build_conv_block(n_channels//2)
+            Fm=self.build_conv_block(n_channels//2, norm_layer, use_bias),
+            Gm=self.build_conv_block(n_channels//2, norm_layer, use_bias)
         )
-        self.rev_block = memcnn.InvertibleModuleWrapper(fn=invertible_module, 
-                                                        keep_input=keep_input, 
-                                                        keep_input_inverse=keep_input)
+        self.invertible_block = memcnn.InvertibleModuleWrapper(fn=invertible_module, 
+                                                               keep_input=keep_input, 
+                                                               keep_input_inverse=keep_input)
 
-    def build_conv_block(self, n_channels):
-        block = nn.Sequential(nn.Conv3d(n_channels, n_channels, kernel_size=5, padding=2),
-                              nn.BatchNorm3d(n_channels),
+    def build_conv_block(self, n_channels, norm_layer, use_bias):
+        block = nn.Sequential(nn.Conv3d(n_channels, n_channels, kernel_size=5, padding=2, bias=use_bias),
+                              norm_layer(n_channels),
                               nn.PReLU(n_channels))
         return block
 
     def forward(self, x, inverse=False):
         if inverse:
-            return self.rev_block.inverse(x)
+            return self.invertible_block.inverse(x)
         else:
-            return self.rev_block(x)
+            return self.invertible_block(x)
 
 
 class InvertibleSequence(nn.Module):
-    def __init__(self, n_channels, n_blocks, keep_input):
+    def __init__(self, n_channels, n_blocks, keep_input, norm_layer, use_bias):
         super(InvertibleSequence, self).__init__()
-        self.sequence = nn.Sequential(*[InvertibleBlock(n_channels, keep_input) for _ in range(n_blocks)])
+        self.sequence = nn.Sequential(*[InvertibleBlock(n_channels, keep_input, norm_layer, use_bias) for _ in range(n_blocks)])
     
     def forward(self, x, inverse=False):
         if inverse:
@@ -87,19 +87,19 @@ class InvertibleSequence(nn.Module):
         for i, block in enumerate(sequence):
             if i == 0:    #https://github.com/silvandeleemput/memcnn/issues/39#issuecomment-599199122
                 if inverse:
-                    block.rev_block.keep_input_inverse = True
+                    block.invertible_block.keep_input_inverse = True
                 else:
-                    block.rev_block.keep_input = True
+                    block.invertible_block.keep_input = True
             x = block(x, inverse=inverse)
         return x        
 
 
 class InputBlock(nn.Module):
-    def __init__(self, out_channels):
+    def __init__(self, out_channels, norm_layer, use_bias):
         super(InputBlock, self).__init__()
         self.out_channels = out_channels
-        self.conv1 = nn.Conv3d(1, out_channels, kernel_size=5, padding=2)
-        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.conv1 = nn.Conv3d(1, out_channels, kernel_size=5, padding=2, bias=use_bias)
+        self.bn1 = norm_layer(out_channels)
         self.relu = nn.PReLU(out_channels)
 
     def forward(self, x):
@@ -110,17 +110,17 @@ class InputBlock(nn.Module):
 
 
 class DownBlock(nn.Module):
-    def __init__(self, in_channels, n_conv_blocks, keep_input):
+    def __init__(self, in_channels, n_conv_blocks, keep_input, norm_layer, use_bias):
         super(DownBlock, self).__init__()
         out_channels = 2*in_channels
-        self.down_conv_ab = self.build_down_conv(in_channels, out_channels)
-        self.down_conv_ba = self.build_down_conv(in_channels, out_channels)
-        self.core = InvertibleSequence(out_channels, n_conv_blocks, keep_input)
+        self.down_conv_ab = self.build_down_conv(in_channels, out_channels, norm_layer, use_bias)
+        self.down_conv_ba = self.build_down_conv(in_channels, out_channels, norm_layer, use_bias)
+        self.core = InvertibleSequence(out_channels, n_conv_blocks, keep_input, norm_layer, use_bias)
         self.relu = nn.PReLU(out_channels)
 
-    def build_down_conv(self, in_channels, out_channels):
-        return nn.Sequential(nn.Conv3d(in_channels, out_channels, kernel_size=2, stride=2),
-                             nn.BatchNorm3d(out_channels),
+    def build_down_conv(self, in_channels, out_channels, norm_layer, use_bias):
+        return nn.Sequential(nn.Conv3d(in_channels, out_channels, kernel_size=2, stride=2, bias=use_bias),
+                             norm_layer(out_channels),
                              nn.PReLU(out_channels))
 
     def forward(self, x, inverse=False):
@@ -135,17 +135,17 @@ class DownBlock(nn.Module):
 
 
 class UpBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, n_conv_blocks, keep_input):
+    def __init__(self, in_channels, out_channels, n_conv_blocks, keep_input, norm_layer, use_bias):
         super(UpBlock, self).__init__()
-        self.up_conv_ab = self.build_up_conv(in_channels, out_channels)
-        self.up_conv_ba = self.build_up_conv(in_channels, out_channels)
+        self.up_conv_ab = self.build_up_conv(in_channels, out_channels, norm_layer, use_bias)
+        self.up_conv_ba = self.build_up_conv(in_channels, out_channels, norm_layer, use_bias)
 
-        self.core = InvertibleSequence(out_channels, n_conv_blocks, keep_input)
+        self.core = InvertibleSequence(out_channels, n_conv_blocks, keep_input, norm_layer, use_bias)
         self.relu = nn.PReLU(out_channels)
     
-    def build_up_conv(self, in_channels, out_channels):
-        return nn.Sequential(nn.ConvTranspose3d(in_channels, out_channels // 2, kernel_size=2, stride=2),
-                             nn.BatchNorm3d(out_channels // 2),
+    def build_up_conv(self, in_channels, out_channels, norm_layer, use_bias):
+        return nn.Sequential(nn.ConvTranspose3d(in_channels, out_channels // 2, kernel_size=2, stride=2, bias=use_bias),
+                             norm_layer(out_channels // 2),
                              nn.PReLU(out_channels // 2))
 
     def forward(self, x, skipx, inverse=False):
@@ -161,10 +161,10 @@ class UpBlock(nn.Module):
 
 
 class OutBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, norm_layer, use_bias):
         super(OutBlock, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size=5, padding=2)
-        self.bn1 = nn.BatchNorm3d(in_channels)
+        self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size=5, padding=2, bias=use_bias)
+        self.bn1 = norm_layer(in_channels)
         self.conv2 = nn.Conv3d(in_channels, 1, kernel_size=1)
         self.relu1 = nn.PReLU(1)
         self.tanh = nn.Tanh() 
--- models/util.py
+++ models/util.py
@@ -5,11 +5,11 @@ from torch.optim import lr_scheduler
 
 
 def get_norm_layer(norm_type='instance'):
-    if norm_type is 'batch':
+    if norm_type == 'batch':
         return functools.partial(nn.BatchNorm3d, affine=True) # TODO: check sync version https://pytorch.org/docs/stable/nn.html#torch.nn.SyncBatchNorm
-    elif norm_type is 'instance':
+    elif norm_type == 'instance':
         return functools.partial(nn.InstanceNorm3d, affine=False, track_running_stats=True)
-    elif norm_type is 'none':
+    elif norm_type == 'none':
         return None
     else:
         raise NotImplementedError('Normalization layer [%s] not supported' % norm_type)
@@ -18,12 +18,12 @@ def is_bias_before_norm(norm_type='instance'):
     """When using BatchNorm, the preceding Conv layer does not use bias, 
     but it does if using InstanceNorm.
     """
-    if norm_type is 'instance' or norm_type is 'none':
+    if norm_type == 'instance' or norm_type == 'none':
         return True
-    elif norm_type is 'batch':
+    elif norm_type == 'batch':
         return False
     else:
-        raise NotImplementedError('Normalization layer [%s] not supported' % nnorm_typeorm_type)
+        raise NotImplementedError('Normalization layer [%s] not supported' % norm_typeorm_type)
 
 def get_scheduler(optimizer, conf):
     """Return a scheduler that keeps the same learning rate for the first <conf.n_epochs> epochs
