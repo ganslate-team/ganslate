b840cd1226c828cc3627094a7c48e2829b141599
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2020-09-21 16:24:23 +0000

    better log setup

--- tools/train.py
+++ tools/train.py
@@ -1,7 +1,4 @@
 import sys
-import torch
-from omegaconf import OmegaConf
-from pathlib import Path
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,51 +10,18 @@ except ImportError:
     sys.path.append('./')
     import midaGAN
 
-from midaGAN.utils import communication
-from midaGAN.utils.environment import setup_logging
 from midaGAN.trainer import Trainer
-from midaGAN.conf import init_config
 # -------------------------------------
-
-
-def train():
-    # experiment_dir = base_directory / run_name
-
+    # TODO: this might come in handy later
     # if communication.get_local_rank() == 0:
         # Want to prevent multiple workers from trying to write a directory
         # This is required in the logging below
         # pass
         # experiment_dir.mkdir(parents=True, exist_ok=True)
     # communication.synchronize()  # Ensure folders are in place.
-
     # log_file = experiment_dir / f'log_{machine_rank}_{communication.get_local_rank()}.txt'
     
-    trainer = Trainer()
-    conf = trainer.conf
-
-    # TODO: clean this mess
-    log_file = Path(conf.logging.checkpoint_dir) / Path('log.txt')
-    debug = False
-    setup_logging(
-        use_stdout=communication.get_local_rank() == 0 or debug,
-        filename=log_file,
-        log_level=('INFO' if not debug else 'DEBUG')
-    )
-    # TODO: all this should be in trainer.py
-    # logger.info(f'Machine rank: {machine_rank}.')
-    logger.info(f'Local rank: {communication.get_local_rank()}.')
-    logger.info(f'Logging: {log_file}.')
-    # logger.info(f'Saving to: {experiment_dir}.')
-    # logger.info(f'Run name: {run_name}.')
-    # logger.info(f'Config file: {cfg_filename}.')
-    logger.info(f'Python version: {sys.version.strip()}.')
-    logger.info(f'PyTorch version: {torch.__version__}.')  # noqa
-    logger.info(f'CUDA {torch.version.cuda} - cuDNN {torch.backends.cudnn.version()}.')
-    logger.info(f'Configuration: {OmegaConf.to_yaml(conf)}.')
-
-
-    trainer.run()
-
 
 if __name__ == '__main__':
-    train()
+    trainer = Trainer()
+    trainer.run()
