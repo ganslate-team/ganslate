6571f13276c4b3048fd5148d20ff00ceab72577e
julianposch <julian.posch@gmail.com>
2021-01-15 20:23:20 +0100

    Added unequal normalization according to piecewise function

--- midaGAN/data/utils/normalization.py
+++ midaGAN/data/utils/normalization.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def min_max_normalize(image, min_value, max_value):
@@ -62,3 +63,141 @@ def get_stats_for_z_score_denormalization(tensor):
 
 def z_score_denormalize():
     pass  # TODO
+
+
+def unequal_normalize(x, min_value, max_value, split_points, split_proportions):
+
+    '''
+    Normalizes given data according to a three-piecewise linear function.
+    '''
+    max_range = max_value - min_value
+
+    def inner(x):
+        # get range up to split point
+        limited_range = split_points[0] - min_value
+        range_proportion = limited_range/max_range
+        # To normalize input to range of [0, 1]
+        x = (x - min_value)/max_range
+        # To normalize according to desired split percentage
+        range_modifier = split_proportions[0]/range_proportion
+        x *= range_modifier
+        return 2*x-1
+
+    def middle(x):
+        # get range between split points
+        limited_range = split_points[1] - split_points[0]
+        range_proportion = limited_range/max_range
+        # To normalize input to range of [0, 1]
+        x = (x - min_value)/max_range
+        # To normalize according to desired split percentage
+        range_modifier = split_proportions[1]/range_proportion
+        x = x*range_modifier
+        # have to add previous piecewise function max and deduct current piecewise function min
+        current_min = (split_points[0]- min_value)/max_range*range_modifier
+
+        range_modifier_inner = split_proportions[0]/((split_points[0] - min_value)/max_range)
+        previous_max = (split_points[0]- min_value)/max_range*range_modifier_inner
+
+        x = x + previous_max - current_min
+        return 2*x-1
+    
+    def outer(x):
+        # get range between split points
+        limited_range = max_value - split_points[1]
+        range_proportion = limited_range/max_range
+        # To normalize input to range of [0, 1]
+        x = (x - min_value)/max_range
+        # To normalize according to desired split percentage
+        range_modifier = split_proportions[2]/range_proportion
+        x = x*range_modifier
+        # have to add previous piecewise functions max and deduct previous and current piecewise function min
+        current_min = (split_points[1]- min_value)/max_range*range_modifier
+
+        range_modifier_inner = split_proportions[0]/((split_points[0] - min_value)/max_range)
+        previous_max_inner = (split_points[0]- min_value)/max_range*range_modifier_inner
+        
+        range_modifier_middle = split_proportions[1]/((split_points[1] - split_points[0])/max_range)
+        previous_max_middle = (split_points[1]- min_value)/max_range*range_modifier_middle
+        previous_min_inner = (split_points[0]- min_value)/max_range*range_modifier_middle
+
+        x = x + previous_max_inner + previous_max_middle - current_min - previous_min_inner
+        return 2*x-1
+
+    result = np.piecewise(x, [x < split_points[0], (x >= split_points[0]) & (x <= split_points[1]), x > split_points[1]], [lambda x: inner(x), lambda x: middle(x), lambda x: outer(x)])
+    return result
+
+
+def unequal_denormalize(x, min_value, max_value, split_points, split_proportions):
+
+    '''
+    Denormalizes given data according to a three-piecewise linear function.
+    '''
+
+    split_point_1 = (2*split_proportions[0]-1)
+    split_point_2 = (2*(split_proportions[1]+split_proportions[0])-1)
+    max_range = max_value - min_value
+
+    def inner(x):
+        # get data to [0,1]
+        x = (x+1)/2
+        # divide by range modifier
+        limited_range = split_points[0] - min_value
+        range_proportion = limited_range/max_range
+        range_modifier = split_proportions[0]/range_proportion
+        x /= range_modifier
+        # denorm from [0,1] normalizer
+        x *= max_range
+        x += min_value
+        return x
+
+    def middle(x):
+        # get data to [0,1]
+        x = (x+1)/2
+        # deduct previous piecewise function max and add current piecewise function min
+        limited_range = split_points[1] - split_points[0]
+        range_proportion = limited_range/max_range
+        range_modifier = split_proportions[1]/range_proportion
+
+        current_min = (split_points[0]- min_value)/max_range*range_modifier
+
+        range_modifier_inner = split_proportions[0]/((split_points[0] - min_value)/max_range)
+        previous_max = (split_points[0]- min_value)/max_range*range_modifier_inner
+
+        x = x - previous_max + current_min
+
+        # Denormalize according to desired split percentage
+        x = x/range_modifier
+
+        # denorm from [0,1] normalizer
+        x *= max_range
+        x += min_value
+        return x
+    
+    def outer(x):
+        # get data to [0,1]
+        x = (x+1)/2
+        # get range between split points and range_modifier
+        limited_range = max_value - split_points[1]
+        range_proportion = limited_range/max_range
+        range_modifier = split_proportions[2]/range_proportion
+
+        # Deduct previous piecewise functions max and add previous and current piecewise function min
+        current_min = (split_points[1]- min_value)/max_range*range_modifier
+
+        range_modifier_inner = split_proportions[0]/((split_points[0] - min_value)/max_range)
+        previous_max_inner = (split_points[0]- min_value)/max_range*range_modifier_inner
+        
+        range_modifier_middle = split_proportions[1]/((split_points[1] - split_points[0])/max_range)
+        previous_max_middle = (split_points[1]- min_value)/max_range*range_modifier_middle
+        previous_min_inner = (split_points[0]- min_value)/max_range*range_modifier_middle
+
+        x = x - previous_max_inner - previous_max_middle + current_min + previous_min_inner
+        # denormalize according to split percentage
+        x = x/range_modifier
+        # denorm from [0,1] normalizer
+        x *= max_range
+        x += min_value
+        return x
+
+    result = np.piecewise(x, [x < split_point_1, (x >= split_point_1) & (x <= split_point_2), x > split_point_2], [lambda x: inner(x), lambda x: middle(x), lambda x: outer(x)])
+    return result
