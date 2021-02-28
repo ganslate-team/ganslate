8cdc5f76f14c9ff3f503e109982eef2604a1b61e
Suraj Pai <suraj.pai@maastro.nl>
2021-02-28 23:30:33 +0530

    Update docker shell script
    Update docker shell script

--- docker/infer/run_inference_pipeline.sh
+++ docker/infer/run_inference_pipeline.sh
@@ -5,3 +5,8 @@ cd ..
 cd midaGAN
 echo "Running inference .."
 conda run python tools/infer.py config=/model/config.yaml infer.checkpointing.load_iter=50000
+
+cd ..
+cd clinical-evaluation
+echo "Converting translated nrrd to dicom"
+conda run python projects/mirada_wbx/generate_dicoms.py /model/infer/saved --output_dir /data/out
